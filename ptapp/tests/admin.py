7b9a15d259b75952f0a5f53ef5330da1b74bd3f7
Aleksei Fomin <aleksei.fomin@arloid.com>
2018-07-17 18:13:08 +0300

    use nested_admin for admin view

--- ptapp/tests/admin.py
+++ ptapp/tests/admin.py
@@ -1,9 +1,24 @@
 from django.contrib import admin
+import nested_admin
 from .models import TestGroup, TestUnit, Question, Answer
 
 # Register your models here.
 
+
+class AnswerInline(nested_admin.NestedTabularInline):
+    model = Answer
+    extra = 4
+
+
+class QuestionInline(nested_admin.NestedStackedInline):
+    model = Question
+    inlines = [AnswerInline]
+    extra = 4
+
+
+class TestUnitAdmin(nested_admin.NestedModelAdmin):
+    inlines = [QuestionInline]
+
+
+admin.site.register(TestUnit, TestUnitAdmin)
 admin.site.register(TestGroup)
-admin.site.register(TestUnit)
-admin.site.register(Question)
-admin.site.register(Answer)
