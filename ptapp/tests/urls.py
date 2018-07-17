9201ee24ee5c24eb4f6bc357bc3008d48b2475cf
Aleksei Fomin <aleksei.fomin@arloid.com>
2018-07-17 18:12:42 +0300

    update views for tests

--- ptapp/tests/urls.py
+++ ptapp/tests/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'test'
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.test_groups, name='groups'),
+    url(r'^(?P<group_id>[0-9]+)/', views.group_tests, name='group'),
 ]
--- ptapp/tests/views.py
+++ ptapp/tests/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.urls import reverse
 
 # Create your views here.
 
@@ -10,13 +11,21 @@ def get_user():
     return User.objects.get(username='fomin')
 
 
-def users(request):
-    return HttpResponse(get_user())
-
-
-def index(request):
+def test_groups(request):
     context = {
-        'test_groups': TestGroup.objects.all(),
+        'list_name': 'Наборы тестов',
+        'button_text': 'Перейти к тестам',
+        'list_object': ((obj, reverse('test:group', args=(obj.id,))) for obj in TestGroup.objects.all()),
     }
-    print(TestGroup.objects.all())
-    return render(request, "tests/index.html", context)
+
+    return render(request, "tests/list_view.html", context)
+
+
+def group_tests(request, group_id):
+    tg = TestGroup.objects.get(id=group_id)
+    context = {
+        'list_name': 'Тесты по теме \"%s\"'%tg.name,
+        'button_text': 'Выполнить тест',
+        'list_object': ((obj, reverse('test:groups')) for obj in tg.testunit_set.all()),
+    }
+    return render(request, "tests/list_view.html", context)
