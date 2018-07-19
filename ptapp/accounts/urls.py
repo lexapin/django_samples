1ce9e3049d56a977636596f07fa541e0585a24fe
Aleksei Fomin <aleksei.fomin@arloid.com>
2018-07-19 19:05:18 +0300

    add accounts app

---
+++ ptapp/accounts/urls.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.views import (
+    LoginView, LogoutView,
+)
+
+from django.conf.urls import url
+
+from . import views
+
+app_name = 'accounts'
+urlpatterns = [
+    url(r'^signup/', views.signup, name='signup'),
+    url(r'^login/', LoginView.as_view(template_name='accounts/signin.html'), name='signin'),
+    url(r'^logout/', LogoutView.as_view(), name='logout'),
+]
