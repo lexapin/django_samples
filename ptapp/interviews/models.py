ee1544764b40bcac0b110a33f7573d2c67d02d57
Aleksei Fomin <aleksei.fomin@arloid.com>
2018-07-20 18:45:07 +0300

    description for interview

--- ptapp/interviews/models.py
+++ ptapp/interviews/models.py
@@ -3,15 +3,29 @@ from django.conf import settings
 from tests.models import TestUnit, Question, Answer
 
 
-# Create your models here.
+# Модели приложения интервью
 
 
 class Interview(models.Model):
+    """
+    Класс опрос
+    содержит информацию о выполняемом пользователем тесте
+    user - пользователь, который проходит опрос
+    testunit - по выбронному тесту
+    is_complete - статус опроса (если все вопросы теста пройдены is_complete=True)
+    """
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     testunit = models.ForeignKey(TestUnit, on_delete=models.CASCADE)
     is_complete = models.BooleanField(default=False)
 
     def get_next_question(self):
+        """
+        - Получает из TestUnit следующий по порядку вопрос
+        - Создает для него пользовательский ответ Reply
+        :return:
+        Reply - Если в тесте есть вопросы
+        None - Если тест завершен
+        """
         reply = self.reply_set.last()
         if reply is None:
             reply = Reply(interview=self, question=self.testunit.question_set.first())
@@ -29,15 +43,32 @@ class Interview(models.Model):
 
 
 class Reply(models.Model):
+    """
+    класс Пользовательский ответ
+    содержит информацию о вопросе
+    is_reply - пройден он пользователем или нет
+    question - вопрос, на который пользователь отвечает (или ответил)
+    interview - к какому опросу относится пользовательский ответ
+    """
     interview = models.ForeignKey(Interview, on_delete=models.CASCADE)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     is_reply = models.BooleanField(default=False)
 
     @property
     def user_answers(self):
+        """
+        Собирает ответы пользователя
+        :return:
+        """
         return sorted([u_answer. answer for u_answer in self.useranswer_set.all()], key=lambda answer: answer.id)
 
 
 class UserAnswer(models.Model):
+    """
+    класс Вариант пользователя
+    вариант ответа на вопрос теста
+    - reply - пользовательский ответ
+    - answer - Выбранный ответ из теста
+    """
     reply = models.ForeignKey(Reply, on_delete=models.CASCADE)
     answer = models.ForeignKey(Answer, on_delete=models.CASCADE, default=1)
--- ptapp/interviews/views.py
+++ ptapp/interviews/views.py
@@ -1,12 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
 
-# Create your views here.
 
-from django.contrib.auth.models import User
-from .models import Interview, Reply, UserAnswer
+# Представление приложения Интервью
+# Состоит из трех представлений
+# - open_interview создает опрос по выбронному пользователем тесту
+#     - создает опрос если он не создан
+#     - не имеет собственного представления
+#     - перенаправляет пользователя на другие представления приложения
+# - question
+#     - отображает текущий вопрос теста
+#     - по зовершении теста перенаправляет к представлению report
+# - report
+#     - отображает краткий и детальный отчет по выполненному тесту
+
+
+from .models import Interview
 from tests.models import TestGroup, TestUnit, Question, Answer
 
 
@@ -16,20 +27,18 @@ def open_interview(request, testunit_id):
     Промежуточное представление:
     - Открывает тест для опроса пользователя
     - смотрит не выполнял ли пользователь данный тест.
-    - Если опрос еще не был пройден пользователем - создает опрос
-    - Если опрос уже пройден - скидывает пользователя к результатам
-    :param request:
-    :return:
+    - Если опрос ранее не был создан - создает опрос и перенаправляет на первый вопрос
+    - Если опрос уже создан, но еще не был пройден пользователем до конца - перенаправляет на вопрос
+    - Если опрос уже пройден - скидывает пользователя к результатам выполненного ранее теста
+    :param request: объект запроса
+    :param testunit_id: идентификатор теста
+    :return: ссылка для перенаправления на представление Вопроса (question) или Отчета (report)
     """
-    testunit = TestUnit.objects.get(pk=testunit_id)
-    user = request.user
+    testunit = get_object_or_404(TestUnit, id=testunit_id)
     try: # Как Вы относитесь к блокам try-except в своих релизах?
-        interview = Interview.objects.get(
-                                  user = user,
-                                  testunit = testunit,
-                                )
+        interview = Interview.objects.get(user = request.user, testunit = testunit,)
     except Interview.DoesNotExist:
-        interview = Interview(user = user, testunit = testunit)
+        interview = Interview(user = request.user, testunit = testunit)
         interview.save()
 
     if interview.is_complete:
@@ -41,17 +50,26 @@ def open_interview(request, testunit_id):
 @login_required
 def question(request, interview_id):
     """
-    Выводит текукщий вопрос
-    :param request:
-    :param interview_id:
+    Представление ВОПРОС
+    Отображает форму для выбора одного или нескольких ответов
+    :param request: объект запроса
+    :param interview_id: идентификатор интервью
     :return:
+    Выводит текукщий вопрос
+        - Если вопросов в тесте больше нет - перенаправляет на результаты
+            - Метод GET:
+                - Выводит представление модели Question
+            - Метод POST:
+                - Для каждого выбранного ответа Answer создает объект UserAnswer и связывает его с Reply
+                    - Считается что пользователь дал ответ - ставится флаг is_reply = True
+                - Пользователь перенаправляется на страницу со следующим вопросом
+                - Если пользователь не дал ответов - перенаправление на текущий вопрос.
     """
-    try:
-        interview = Interview.objects.get(id = interview_id, user = request.user)
-    except Interview.DoesNotExist:
-        return HttpResponse('Вау!!!')
+    interview = get_object_or_404(Interview, id = interview_id, user = request.user)
     question_for_reply = interview.get_next_question()
     if question_for_reply is None:
+        interview.is_complete = True
+        interview.save()
         return HttpResponseRedirect(reverse('interview:report', args=(interview.id,)))
     if request.method == 'POST':
         for answer in question_for_reply.question.answer_set.all():
@@ -62,7 +80,8 @@ def question(request, interview_id):
             question_for_reply.save()
             return HttpResponseRedirect(reverse('interview:question', args=(interview.id,)))
         else:
-            print('Не выбраны ответы')
+            pass
+            #TODO: Добавить сообщение на форме о том, что ответы не выбраны
     context = {
         'question': question_for_reply.question,
     }
@@ -71,10 +90,15 @@ def question(request, interview_id):
 
 @login_required
 def report(request, interview_id):
-    try:
-        interview = Interview.objects.get(id = interview_id, user = request.user)
-    except Interview.DoesNotExist:
-        return HttpResponse('Вау!!!')
+    """
+    Представление отчет
+    После завершения теста (interview.get_next_question() is None)
+    Собирает и сопоставляет ответы пользователя на вопросы теста
+    :param request: объект запроса
+    :param interview_id: идентификатор интервью
+    :return: Возвращает результаты выполненного теста
+    """
+    interview = get_object_or_404(Interview, id=interview_id, user=request.user, is_complete=True)
     count_right = 0
     count_all = interview.reply_set.count()
     detail = []
