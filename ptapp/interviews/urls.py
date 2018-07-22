ca815243f457b8060d1c2cfa884477b571bc28ca
Aleksei <aleksei.fomin@arloid.com>
2018-07-22 03:52:02 +0300

    change view method for question

--- ptapp/interviews/urls.py
+++ ptapp/interviews/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = 'interview'
 urlpatterns = [
     url(r'^test/(?P<testunit_id>[0-9]+)/', views.open_interview, name='open'),
-    url(r'^(?P<interview_id>[0-9]+)/question', views.question, name='question'),
+    url(r'^(?P<pk>[0-9]+)/question', views.QuestionView.as_view(), name='question'),
     url(r'^(?P<interview_id>[0-9]+)/report', views.report, name='report'),
 ]
--- ptapp/interviews/views.py
+++ ptapp/interviews/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
 
+from django.views.generic.detail import DetailView
+from django.utils.decorators import method_decorator
 
 # Представление приложения Интервью
 # Состоит из трех представлений
@@ -42,50 +43,9 @@ def open_interview(request, testunit_id):
         interview.save()
 
     if interview.is_complete:
-        return HttpResponseRedirect(reverse('interview:report', args=(interview.id,)))
+        return redirect(reverse('interview:report', args=(interview.id,)))
     else:
-        return HttpResponseRedirect(reverse('interview:question', args=(interview.id,)))
-
-
-@login_required
-def question(request, interview_id):
-    """
-    Представление ВОПРОС
-    Отображает форму для выбора одного или нескольких ответов
-    :param request: объект запроса
-    :param interview_id: идентификатор интервью
-    :return:
-    Выводит текукщий вопрос
-        - Если вопросов в тесте больше нет - перенаправляет на результаты
-            - Метод GET:
-                - Выводит представление модели Question
-            - Метод POST:
-                - Для каждого выбранного ответа Answer создает объект UserAnswer и связывает его с Reply
-                    - Считается что пользователь дал ответ - ставится флаг is_reply = True
-                - Пользователь перенаправляется на страницу со следующим вопросом
-                - Если пользователь не дал ответов - перенаправление на текущий вопрос.
-    """
-    interview = get_object_or_404(Interview, id = interview_id, user = request.user)
-    question_for_reply = interview.get_next_question()
-    if question_for_reply is None:
-        interview.is_complete = True
-        interview.save()
-        return HttpResponseRedirect(reverse('interview:report', args=(interview.id,)))
-    if request.method == 'POST':
-        for answer in question_for_reply.question.answer_set.all():
-            if str(answer.id) in request.POST:
-                question_for_reply.useranswer_set.create(answer = answer)
-        if question_for_reply.useranswer_set.count():
-            question_for_reply.is_reply = True
-            question_for_reply.save()
-            return HttpResponseRedirect(reverse('interview:question', args=(interview.id,)))
-        else:
-            pass
-            #TODO: Добавить сообщение на форме о том, что ответы не выбраны
-    context = {
-        'question': question_for_reply.question,
-    }
-    return render(request, "interviews/question.html", context)
+        return redirect(reverse('interview:question', args=(interview.id,)))
 
 
 @login_required
@@ -116,3 +76,63 @@ def report(request, interview_id):
         'detail': detail,
     }
     return render(request, "interviews/report.html", context)
+
+
+class QuestionView(DetailView):
+    """
+    Класс представление модели ВОПРОС (Question)
+    - Отображает форму для выбора одного или нескольких ответов ('экземпляр модели Question
+                со связанными с ней экземплярами модели Answer)
+    - Перенаправляет пользователя к представлению результаты, если вопросы в тесте закончились
+    - Метод POST:
+        - Для каждого выбранного ответа Answer создает объект UserAnswer и связывает его с Reply
+            - Считается что пользователь дал ответ - ставится флаг is_reply = True
+        - Пользователь перенаправляется на страницу со следующим вопросом
+        - Если пользователь не дал ответов - перенаправление на текущий вопрос.
+    НЕ САМОЕ ЛУЧШЕЕ РЕШЕНИЕ, ХОТЕЛОСЬ БЫ УЗНАТЬ ПРАВИЛЬНЫЙ ОТВЕТ (как сделать лучше)
+    """
+    model = Question
+    template_name = "interviews/question.html"
+
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        """
+        Диспетчер:
+        - принимает запросы авторизованных пользователей
+        - Ищет интервью которое задано в URLе
+            - если интервью в базе отсутствует -> 404
+            - иначе в экземпляре представления создает атрибут question_for_reply 
+                    для дальнейшей работы
+        """
+        interview = get_object_or_404(Interview, pk = kwargs['pk'], user = request.user)
+        question_for_reply = interview.get_next_question()
+        if question_for_reply is None:
+            interview.is_complete = True
+            interview.save()
+            return redirect(reverse('interview:report', args=(interview.id,)))
+        else:
+            self.question_for_reply = question_for_reply
+        return super(QuestionView, self).dispatch(request, *args, **kwargs)
+
+    def get_object(self, queryset=None):
+        """
+        переггрузка метода get_object
+        не используем queryset для получения ВОПРОСА
+        а просто получаем его из question_for_reply
+        """
+        return self.question_for_reply.question
+
+    def post(self, request, *args, **kwargs):
+        """
+        Обработка ответа пользователя
+        """
+        for answer in self.question_for_reply.question.answer_set.all():
+            if str(answer.id) in request.POST:
+                self.question_for_reply.useranswer_set.create(answer = answer)
+        if self.question_for_reply.useranswer_set.count():
+            self.question_for_reply.is_reply = True
+            self.question_for_reply.save()
+            return redirect(reverse('interview:question', args=(kwargs['pk'],)))
+        else:
+            pass
+            #TODO: Добавить сообщение на форме о том, что ответы не выбраны
